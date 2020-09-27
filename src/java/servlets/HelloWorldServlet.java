54b7fa86e6854222e984fd32c5ba7de98f78fc75
nmik2111 <naeunmik@gmail.com>
2020-09-27 06:10:47 -0600

    Improved validation Added a message for the user.

--- src/java/servlets/HelloWorldServlet.java
+++ src/java/servlets/HelloWorldServlet.java
@@ -30,6 +30,7 @@ public class HelloWorldServlet extends HttpServlet {
         request.setAttribute("lastname", lastname);
         
         if (firstname == null || firstname.equals("") || lastname == null || lastname.equals("")) {
+            request.setAttribute("message", "Invalid entry. Please enter both your first and last names.");
             getServletContext().getRequestDispatcher("/WEB-INF/helloWorldForm.jsp")
                 .forward(request, response);
             return;
